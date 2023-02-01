e28bb430ec67d8920b9ffbe89c43ee70ec6c2497
Amal C P <acp070146@gmail.com>
2023-02-01 20:03:05 +0530

    chat option started

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,16 +2,31 @@
 import logo from '../images/logo-bw.svg'
 import colorlogo from '../images/logo-wr.svg'
 
-const Nav = ({minimal,authToken}) => {
+const Nav = ({ authToken, minimal, setShowModal, showModal, setIsSignUp }) => {
+  const handleClick = () => {
+    setShowModal(true);
+    setIsSignUp(false);
+  };
 
   return (
-  <nav>
-    <div className="logo-container">
-        <img className="logo" src={minimal ? colorlogo : logo} alt="" />
-    </div>
-    {!authToken && !minimal && <button className='nav-button'>Log In</button>}
-  </nav>
+    <nav>
+      <div className="logo-container">
+        <img
+          className="logo"
+          src={minimal ? colorlogo : logo}
+          alt="logo"
+        />
+      </div>
+      {!authToken && !minimal && (
+        <button
+          className="nav-button"
+          onClick={handleClick}
+          disabled={showModal}
+        >
+          Log in
+        </button>
+      )}
+    </nav>
   );
 };
-
 export default Nav;
